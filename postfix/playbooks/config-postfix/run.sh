07286d48275cc4a56955f28f2dee3a04ecb5ec2f
bbs-md <bbs.md@mail.ru>
2023-03-08 18:32:36 +0200

    Lesson-50 Changed run.sh

--- postfix/playbooks/config-postfix/run.sh
+++ postfix/playbooks/config-postfix/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 set -e
-ansible-playbook ./site.yaml -i ../../inventory/ -e @secrets-file.enc --ask-vault-pass
+inventory="../../my-inventory/"
+FILE=$1
+if [ -f "$FILE" ] || [ -d "$FILE" ]; then
+    if [ $FILE != "." ] && [ $FILE != ".." ]; then
+        inventory=$FILE
+    fi
+fi
+echo "file [$inventory] is being used as inventory."
+
+ansible-playbook ./site.yaml -i "$inventory" -e @secrets-file.enc --ask-vault-pass
